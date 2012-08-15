ee5f9815c9bbaeb4f23fcb3b8a0d0fb3aa29097f
huangxiaobo.routon@gmail.com <huangxiaobo.routon@gmail.com@cdea0e49-4e81-d697-b58f-f95e96613c0c>
2012-08-15 12:17:21 +0000

    git-svn-id: https://amw-python-study.googlecode.com/svn/trunk@17 cdea0e49-4e81-d697-b58f-f95e96613c0c

--- c100/01.py
+++ c100/01.py
@@ -1,32 +1,23 @@
 #!/usr/bin/python
 #!-*- coding:utf-8 -*-
-
 '''
 【程序1】
 题目：有1、2、3、4个数字，能组成多少个互不相同且无重复数字的三位数？都是多少？
 '''
 
+import os
+import sys
+import itertools
+
 if __name__ == '__main__':
     l = (1, 2, 3, 4)
-    no = []
-    s = set()
+    r = []
 
-    for x in range(len(l)):
-        no.append(x)
-        for y in range(len(l)):
-            if y not in no:
-                no.append(y)
-                for z in range(len(l)):
-                    if z not in no:
-                        no.append(z)
-                        s.add(l[no[0]]*100 + l[no[1]]*10 + l[no[2]])
-                        no.pop()
-                no.pop()
-        no.pop()
+    for i in itertools.permutations(range(1, 5), 3):
+        r.append(eval('%d*100 + %d*10 + %d' % i))
 
-    print '总共有：',len(s)
-    print '如下：', s
-                        
+    print '共有：', len(r)
+    print '排序如下：', r
 
     
 
