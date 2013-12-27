d62608cfa3e79e2bc1c9b6778b5d0bb0a28215fa
amw <amw@amw-pc.(none)>
2013-12-28 00:16:42 +0800

    解析number

--- JsonParser/JsonParserv1.py
+++ JsonParser/JsonParserv1.py
@@ -42,7 +42,6 @@ def parse_string(s, pos):
     匹配一个string s必须以‘"’开始
     返回匹配的字符串和与起始'"'匹配的后一位
     '''
-    print s, pos
     print type(s)
 
     while s[pos] in STRIP_TAB:
@@ -72,17 +71,16 @@ def parse_int(s, pos):
     '''
     匹配一个int s必须以‘{’开始
     '''
-    print s, pos
+    pass
 
 def parse_float(s, pos):
     '''
     匹配一个float s必须以‘{’开始
     '''
-    print s, pos
+    pass
 
 @print_caller
 def parse_key(s, pos):
-    print s, pos
 
     val, pos = parse_scan(s, pos)
 
@@ -99,7 +97,6 @@ def parse_object(s, pos):
     '''
     匹配一个dict s必须以‘{’开始
     '''
-    print s, pos
 
     while s[pos] in STRIP_TAB:
         pos += 1
@@ -142,7 +139,7 @@ def parse_object(s, pos):
 
 
 def parse_item(s, pos):
-    print s, pos
+    pass
 
 def parse_array(s, pos):
     '''
@@ -168,9 +165,72 @@ def parse_array(s, pos):
     return result, pos + 1
 
 
+def parse_number(s, pos):
+    neg = False
+
+    while s[pos] in STRIP_TAB:
+        pos += 1
+
+    if s[pos] == u'-':
+        neg = True
+        pos += 1
+    if s[pos] == u'+':
+        neg = False
+        pos += 1
+
+    result = 0
+    #整数部分
+    while s[pos] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
+        result *= 10
+        result += eval(s[pos])
+        pos += 1
+
+    #小数点
+    if s[pos] == u'.':
+        pos += 1
+        result = float(result)
+
+    if neg:
+        result = -result
+
+    base = -1
+    while s[pos] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
+        tmp = eval(s[pos]) * (10 ** base)
+        result += tmp
+        base -= 1
+        pos += 1
+
+    if s[pos] in [u'e', u'E']:
+        pos += 1
+        exp_val = 0
+        exp_neg = False
+        if s[pos] == u'+':
+            exp_neg = False
+            pos += 1
+        elif s[pos] == u'-':
+            exp_neg = True
+            pos += 1
+
+        while s[pos] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
+            exp_val *= 10
+            exp_val += eval(s[pos])
+            pos += 1
+        if exp_neg:
+            exp_val = -exp
+        result = base * (10 ** exp_val)
+
+    return result, pos+1
+
+
+
+
+
+
+
+
 @print_caller
 def parse_scan(s, pos):
-    print s, pos
+    pass
 
     while s[pos] in STRIP_TAB:
         pos += 1
@@ -190,6 +250,7 @@ def parse_scan(s, pos):
     else:
         pass
     print '可能是数字'
+    return parse_number(s, pos)
 
 
 class JsonParser:
