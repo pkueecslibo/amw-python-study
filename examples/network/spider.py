34768d6e41e7b8b37580d560048776dc83e54764
huangxiaobo <huangxb7818@gmail.com>
2014-05-01 22:48:14 +0800

    做一个网络爬虫

---
+++ examples/network/spider.py
@@ -0,0 +1,10 @@
+class Spider :
+	def __init__(self):
+		pass
+
+	def run(self):
+		pass
+
+if __name__ == "__main__":
+	spider = Spider()
+	spider.run()
---
+++ examples/network/tcpserver.py
@@ -0,0 +1,18 @@
+
+
+class TcpServer:
+	def __init__(self):
+		self.host = "localhost"
+		self.port = 5000
+
+	def setup(self):
+		pass
+
+	def __try_connect__(self):
+		pass
+
+	def bind(self):
+		pass
+
+	def run(self):
+		pass
